4bbfbbc0c21d296096f7f743e818467c932d1f9b
volmar86 <146341416+volmar86@users.noreply.github.com>
2026-03-01 11:19:16 +0100

    Add 'sui' to tokens and symbols lists

--- update_prices.py
+++ update_prices.py
@@ -13,15 +13,15 @@ CRYPTO_IDS = [
     'terra-luna', 'tokenfi', 'gains-network', 'reserve-rights-token',
     'woo-network', 'axie-infinity', 'layer3', 'moonveil', 'rivalz-network',
     'lingo', 'lumia', 'zeus-network', 'sidus', 'my-lovely-coin', 'carv',
-    'bluwhale', 'zero-gravity', 'machina'
+    'bluwhale', 'zero-gravity', 'machina', 'sui'
 ]
-
 SYMBOLS = [
     'ETH', 'BNB', 'SOL', 'AVAX', 'TIA', 'OM', 'NEAR', 'SEI', 'ARB', 'GMX',
     'FLOKI', 'MANTA', 'FET', 'NOT', 'ATH', 'GALA', 'VENOM', 'FOXY', 'SHRAP',
     'COREUM', 'ASTER', 'FITFI', 'UOS', 'BRISE', 'NATIX', 'SOIL', 'SENATE',
     'MUBI', 'LUNC', 'TOKEN', 'GNS', 'RSR', 'WOO', 'AXS', 'L3', 'MORE', 'RIZ',
-    'LINGO', 'LUMIA', 'ZEUS', 'SIDUS', 'MLC', 'CARV', 'BLUAI', '0g', 'MXNA'
+    'LINGO', 'LUMIA', 'ZEUS', 'SIDUS', 'MLC', 'CARV', 'BLUAI', '0g', 'MXNA',
+    'SUI'
 ]
 
 def fetch_prices():
